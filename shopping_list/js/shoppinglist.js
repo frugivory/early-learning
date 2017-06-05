c925f4c71a36bac25c7a498f06bacf539129ed0e
dylanebmorris <DylanMorris@Dylans-MacBook-Pro.local>
2017-06-04 22:05:22 -0700

    hover effects on button, background <div>, jQuery to clear the form after click()

--- shopping_list/js/shoppinglist.js
+++ shopping_list/js/shoppinglist.js
@@ -3,11 +3,15 @@ $(document).ready(function() {
   $("#button").click(function() {
     var toAdd = $("input[id=checkListItem]").val();
     $('#shoppingList').append('<li id="item">' + toAdd + '</li>');
+    $("form").trigger("reset"); //this clears the text entered in the form when submitted
+    //$("form")[0].reset(); //this also clears the text entered in the form when submitted https://stackoverflow.com/questions/8701812/clear-form-after-submission-with-jquery
   });
 
   $("#checkListItem").keypress(function(e) {
-    if (e.which == 13) {
+    if (e.which == 13) { //enter key!
       $("#button").click();
+       if(e.preventDefault) e.preventDefault(); // to prevent defaul behaviour of starting a new line when enter is pressed: https://stackoverflow.com/questions/31245808/clear-textarea-input-after-enter-key-press
+        return false; // Just a workaround for old browsers
     }
   });
 
