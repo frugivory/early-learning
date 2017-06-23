bfcf2e741f4daef52bcaab345ae37e36a41df6c8
dylan <DylanMorris@Dylans-MacBook-Pro.local>
2017-06-22 20:41:43 -0700

    name length works now

--- docs/first_ruby/ruby/name_length.rb
+++ docs/first_ruby/ruby/name_length.rb
@@ -1,1 +1,13 @@
-name_length
+puts "I am gonna tell you how long your name is"
+
+puts "what is your first name?"
+first_name = gets.chomp
+
+puts "what is your middle name?"
+middle_name = gets.chomp
+
+puts "what is your last name?"
+last_name = gets.chomp
+
+name_length = (first_name+middle_name+last_name).to_s.length
+puts name_length
