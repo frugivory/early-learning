21d1a587ab65ca4132f7e2611029deebfb1698e8
dylan <DylanMorris@ip-10-10-3-223.us-west-2.compute.internal>
2017-08-01 15:41:53 -0700

    changing some wording

--- docs/first_ruby/ruby/quadratic.rb
+++ docs/first_ruby/ruby/quadratic.rb
@@ -1,4 +1,4 @@
-puts "let us solve your quadratic problems:"
+puts "let us solve your quadratic problem:"
 puts "what is a?"
 a = gets.chomp.to_i
 
@@ -14,7 +14,7 @@ c = gets.chomp.to_i
 soln1 = ((-b+(b*b - 4*a*c)**(0.5))/2*a).to_s
 soln2 = ((-b-(b*b - 4*a*c)**(0.5))/2*a).to_s
 
-puts "x = "+soln1+" or "+soln2
+puts "well, x = "+soln1+" or "+soln2+" I hope that's right..."
 
 else puts "not a quadratic function, try again ding-dong"
 end
